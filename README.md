9c4be6a40c55c237690e432b0b800c1311110cd8
JulianLz96 <jj_lz96@hotmail.com>
2019-07-10 09:49:49 -0500

    primer commit, layout game

---
+++ README.md
@@ -0,0 +1,2 @@
+# Codigo_clase2
+# Verano_Game
---
+++ app/src/main/java/com/iteso/game/MainActivity.java
@@ -0,0 +1,34 @@
+package com.iteso.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.main_activty_startbtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.bounce);
+                startBtn.startAnimation(animation);
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+    }
+}
